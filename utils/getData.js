bb47bf989eaadc3dd839301cc3b36afef6a4fbb3
Daniel Eden <dan.eden@me.com>
2021-01-18 18:14:23 +0000

    Update getData.js

--- utils/getData.js
+++ utils/getData.js
@@ -1,4 +1,6 @@
 export default async function getData(origin) {
+  // Note that this timeout promise is only to demonstrate/simulate network latency.
+  // You could just as easily return a plain object directly!
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({
